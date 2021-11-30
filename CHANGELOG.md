1dd47f84a7813524a86be4005af94fd1194a9394
Martin <github@amusingtime.uk>
2021-11-30 11:18:22 +0000

    Minimal version extracted from realm-works-import module

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 1.0
+
+Make its own module, separated out from the realm-works-import module
---
+++ disguise-unreachable-links.mjs
@@ -0,0 +1,57 @@
+//
+// Simple hooks to prevent links being displayed as links if the target of the link isn't OBSERVABLE by the player.
+//
+const _EntityMap = {
+	"JournalEntry" : "journal",
+	"Actor"        : "actors",
+	"RollTable"    : "tables",
+	"Scene"        : "scenes",
+};
+
+/**
+ * For any link in the text which points to a document which is not visible to the current player
+ * it will be replaced by the non-link text (so the player will be NOT aware that a link exists)
+ * @param {ActorSheet} [sheet] Sheet for renderJournalSheet and renderActorSheet hooks
+ * @param {jQuery}     [html]  HTML  for renderJournalSheet and renderActorSheet hooks
+ * @param {Object}     [data]  Data for renderJournalSheet and renderActorSheet hooks
+ */
+function _checkRenderLinks(sheet, html, data) {	
+	// sheet = ActorSheet
+	// html  = jQuery
+	// data  = object
+	
+	// Original link:
+	//     <a class="entity-link" draggable="true" [ data-entity="JournalEntry" | data-pack="packname" ] data-id=".....">
+	//     <i class="fas fa-th-list">::before</i>
+	//     plain text
+	//     </a>
+	// If the "data-id" isn't observable by the current user, then replace with just "plain text"
+	html.find("a.entity-link").filter( (index,a) => {
+		// This filter function needs to return true if the link is to be replaced by normal text
+		const dataentity = a.getAttribute('data-entity');	// RollTable, JournalEntry, Actor
+		if (!dataentity) {
+			// Compendium packs are only limited at the PACK level, not an individual document level
+			return game.packs.get(a.getAttribute('data-pack'))?.private;
+		}
+		const entity = _EntityMap[dataentity];
+		if (!entity) {
+			console.warn(`checkRenderLinks#EntityMap does not have '${entity}'`);
+			return false;
+		}
+		const item = game[entity].get(a.getAttribute("data-id"));
+		return !item || !item.testUserPermission(game.user, "LIMITED");
+	}).replaceWith ( (index,a) => {
+		const pos = a.indexOf("</i> ");
+		return (pos<0) ? a : a.slice(pos+5);
+	});
+}
+
+Hooks.once('ready', () => {
+	// Only check for link visibility if NOT a gm
+	console.warn(`disguise unreachable links: ready hook`);
+	if (!game.user.isGM) {
+		console.warn(`Adding hooks to disguise unreachable links`);
+		Hooks.on("renderJournalSheet", _checkRenderLinks);
+		Hooks.on("renderActorSheet",   _checkRenderLinks);
+	}
+})
